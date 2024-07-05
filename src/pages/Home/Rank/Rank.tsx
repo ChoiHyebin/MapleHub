79a2bf8a7398184d5baca6614fe4557f0b413006
ChoiHyebin <bini0819@catholic.ac.kr>
2024-07-05 16:55:23 +0900

    feat: 랭킹 정보 조회 API 연동

--- src/pages/Home/Rank/Rank.tsx
+++ src/pages/Home/Rank/Rank.tsx
@@ -1,16 +1,53 @@
+import React from 'react';
+import axios from 'axios';
 import RankCard from './RankCard';
-
 import './Rank.css';
-
 import { FaAngleRight } from 'react-icons/fa6';
 
 const Rank: React.FC = () => {
+  const [rankData, setRankData] = React.useState<any[]>([]);
+
+  React.useEffect(() => {
+    const fetchRankData = async () => {
+      try {
+        const currentDate = new Date().toISOString().slice(0, 10);
+        const response = await axios.get(
+          'https://open.api.nexon.com/maplestory/v1/ranking/overall',
+          {
+            headers: {
+              'x-nxopen-api-key': '',
+            },
+            params: {
+              date: currentDate,
+            },
+          }
+        );
+
+        setRankData(response.data.ranking);
+      } catch (error) {
+        console.error('랭킹 조회 API 호출 실패:', error);
+      }
+    };
+
+    fetchRankData();
+  }, []);
+
   return (
     <div className="rank-container">
       <h2>랭킹 TOP 10</h2>
 
       <div className="rank-list">
-        <RankCard />
+        {rankData.slice(0, 10).map((rank, index) => (
+          <div key={index}>
+            <RankCard
+              rank={rank.ranking}
+              characterName={rank.character_name}
+              characterClass={rank.class_name}
+              characterLevel={rank.character_level}
+              worldName={rank.world_name}
+            />
+          </div>
+        ))}
       </div>
 
       <a href="">
--- src/pages/Home/Rank/RankCard.tsx
+++ src/pages/Home/Rank/RankCard.tsx
@@ -1,22 +1,67 @@
 import './RankCard.css';
 
-const RankCard: React.FC = () => {
+import Scania from '../../../assets/icons/scania.png';
+import Bera from '../../../assets/icons/bera.png';
+import Luna from '../../../assets/icons/luna.png';
+import Zenith from '../../../assets/icons/zenith.png';
+import Croa from '../../../assets/icons/croa.png';
+import Union from '../../../assets/icons/union.png';
+import Elysium from '../../../assets/icons/elysium.png';
+import Enosis from '../../../assets/icons/enosis.png';
+import Red from '../../../assets/icons/red.png';
+import Aurora from '../../../assets/icons/aurora.png';
+import Arcane from '../../../assets/icons/arcane.png';
+import Nova from '../../../assets/icons/nova.png';
+
+interface RankCardProps {
+  rank: number;
+  characterName: string;
+  characterClass: string;
+  characterLevel: number;
+  worldName: string;
+}
+
+const worldImages: any = {
+  스카니아: Scania,
+  베라: Bera,
+  루나: Luna,
+  제니스: Zenith,
+  크로아: Croa,
+  유니온: Union,
+  엘리시움: Elysium,
+  이노시스: Enosis,
+  레드: Red,
+  오로라: Aurora,
+  아케인: Arcane,
+  노바: Nova,
+};
+
+const RankCard: React.FC<RankCardProps> = ({
+  rank,
+  characterName,
+  characterClass,
+  characterLevel,
+  worldName,
+}) => {
+  const worldImage = worldImages[worldName] || null;
+
   return (
     <div className="rankcard-container">
-      <p>서버</p>
+      {/* <p>{worldName}</p> */}
+      {worldImage ? <img src={worldImage} className="world-img" /> : <p>{worldName}</p>}
 
       <div>
         <p>캐릭터 이미지</p>
 
-        <div>
+        <div className="rankcard-info">
           <p>
-            <b>캐릭터 이름</b>
+            <b>{characterName}</b>
             <br />
-            <span>캐릭터 직업</span>
+            <span>{characterClass}</span>
           </p>
 
           <p>
-            <b>캐릭터 레벨</b>
+            <b>{characterLevel}</b>
             <br />
             <span>Level</span>
           </p>
