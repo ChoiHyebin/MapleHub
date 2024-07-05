4a8945ad3fddee35b810233d58c4ff3bd9b35762
ChoiHyebin <bini0819@catholic.ac.kr>
2024-07-05 11:20:00 +0900

    feat: Rank, RankCard 컴포넌트 구현

--- src/pages/Home/Rank/Rank.tsx
+++ src/pages/Home/Rank/Rank.tsx
@@ -1,5 +1,9 @@
 import RankCard from './RankCard';
 
+import './Rank.css';
+
+import { FaAngleRight } from 'react-icons/fa6';
+
 const Rank: React.FC = () => {
   return (
     <div className="rank-container">
@@ -8,6 +12,10 @@ const Rank: React.FC = () => {
       <div className="rank-list">
         <RankCard />
       </div>
+
+      <a href="">
+        랭킹 더 보기 <FaAngleRight fill="#4FB6FF" />
+      </a>
     </div>
   );
 };
--- src/pages/Home/Rank/RankCard.tsx
+++ src/pages/Home/Rank/RankCard.tsx
@@ -1,5 +1,29 @@
+import './RankCard.css';
+
 const RankCard: React.FC = () => {
-  return <div className="rankcard-container">RankCard</div>;
+  return (
+    <div className="rankcard-container">
+      <p>서버</p>
+
+      <div>
+        <p>캐릭터 이미지</p>
+
+        <div>
+          <p>
+            <b>캐릭터 이름</b>
+            <br />
+            <span>캐릭터 직업</span>
+          </p>
+
+          <p>
+            <b>캐릭터 레벨</b>
+            <br />
+            <span>Level</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RankCard;
